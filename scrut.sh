6dd5e2328c445d821ad4b857f0df5613d788b208
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-28 16:47:38 +0100

    Git aply 17

--- scrut.sh
+++ scrut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-41bb-a1d7-f7e208bc5d9c.diff?s=77733959b98bc633370442396c478c5122af8885 \
-    | git apply -v 
+    | git apply -v scrutinizer.patch
