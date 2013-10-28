8bf75798ee660aaf870f2409af77fdcc6dfe29fe
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-28 15:59:21 +0100

    Git Apply 1

--- scrut.sh
+++ scrut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/6b663854-8238-432b-89ae-a09359b76b06.diff?s=0dfd19d651e3fe246039feebfd881e27f02a6261 \
-    git apply 
+    git apply -v
