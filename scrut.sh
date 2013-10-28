56e3c12a485a112bcfe389363ec500b3faf8d8a2
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-28 17:03:25 +0100

    Git Apply 20

--- scrut.sh
+++ scrut.sh
@@ -2,5 +2,3 @@
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-41bb-a1d7-f7e208bc5d9c.diff?s=77733959b98bc633370442396c478c5122af8885 \
     | git apply -v
-	git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
-    git push > /dev/null
