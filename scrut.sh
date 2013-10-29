cdaf306f45f1d86432a7c420017bd20477b9c9e3
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-29 10:02:01 +0100

    Git AM + Commit

--- scrut.sh
+++ scrut.sh
@@ -5,6 +5,8 @@ git config --global push.default matching
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-41bb-a1d7-f7e208bc5d9c.diff?s=77733959b98bc633370442396c478c5122af8885 \
     | git am -
+	
+	git commit
 
 	
 
