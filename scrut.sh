35a24180cc020ee76e95d0ee93d7ebc7d250e0af
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-29 08:54:35 +0100

    Git apply 23

--- scrut.sh
+++ scrut.sh
@@ -4,14 +4,4 @@ curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-4
     | git apply -v
 
 	
-	# go to home and setup git
-        cd $HOME
-        git config --global user.email "aurelien.lefebvre@viacesi.fr"
-		git config --global user.name "Travis"
-        git config --global push.default matching
-       
-        # using token clone gh-pages branch
-        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git repo > /dev/null || error_exit "Error cloning the repository";
-		git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
-		git push > /dev/null
 
