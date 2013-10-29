df945f28b97a96883beb2822c2c4ba9c7c3b1e92
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-29 08:50:33 +0100

    Git Apply 22

--- scrut.sh
+++ scrut.sh
@@ -2,6 +2,7 @@
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-41bb-a1d7-f7e208bc5d9c.diff?s=77733959b98bc633370442396c478c5122af8885 \
     | git apply -v
+
 	
 	# go to home and setup git
         cd $HOME
@@ -11,3 +12,6 @@ curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/76ed71c6-f5cd-4
        
         # using token clone gh-pages branch
         git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git repo > /dev/null || error_exit "Error cloning the repository";
+		git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master"
+		git push > /dev/null
+
