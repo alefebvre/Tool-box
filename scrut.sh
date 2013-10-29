709aa592d50aa4dc258d34a09f239ecb57332666
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-29 10:58:40 +0100

    Wget + Git apply + git diff + push

--- scrut.sh
+++ scrut.sh
@@ -3,9 +3,10 @@ git config --global user.email "aurelien.lefebvre@viacesi.fr"
 git config --global user.name "Travis"
 git config --global push.default matching
 
-wget -q -O https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/b804ffab-9816-4828-9893-8b8d3f07f30c.diff?s=1a92acd8d95f2ec043e6e3bb7b79096f0327389b \
+wget -q -O - https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/b804ffab-9816-4828-9893-8b8d3f07f30c.diff?s=1a92acd8d95f2ec043e6e3bb7b79096f0327389b \
     | git apply -v
 	
+	git diff
 	
 	git push  https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git
 	
