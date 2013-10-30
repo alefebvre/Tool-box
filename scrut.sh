ddb7c3cf05bc92613caf8a77365d1e34bf9e7b47
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-30 11:38:54 +0100

    Git Apply

--- scrut.sh
+++ scrut.sh
@@ -4,7 +4,7 @@ git config --global user.name "Travis"
 git config --global push.default matching
 
 curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/b804ffab-9816-4828-9893-8b8d3f07f30c.diff?s=1a92acd8d95f2ec043e6e3bb7b79096f0327389b  \
-    | git apply -v
+    | git apply -
 	git add *
 	git commit -m "cs-fixer"
 	git push  https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git
