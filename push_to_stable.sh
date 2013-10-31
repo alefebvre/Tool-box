5bf7ec97cfcc1f4cd09a8d1165f0d63fbbe3ad85
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-31 16:06:11 +0100

    Try 1

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -28,7 +28,7 @@
 
         # add, commit and push files
 		
-        git add *
+        git add .
         git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to stable"
         git push > /dev/null
         echo -e "Pushed to GitHub"
