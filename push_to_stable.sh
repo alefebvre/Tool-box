1f99c951e5f894da767ae350e3bfdbbf3592f8c5
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-31 16:54:46 +0100

    Git pull 8

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -3,12 +3,7 @@
 		cp -Rv Application $HOME/Application/
         cp -Rv test $HOME/test/
        
-	    cd $HOME
-        git config --global user.email "aurelien.lefebvre@viacesi.fr"
-		git config --global user.name "Travis"
-        git config --global push.default matching
-       
-        git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git
+	  
      	
         git checkout stable
         
