c53b8421f236ee94ae903785767ed9959c902e89
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-10-31 16:37:09 +0100

    Git pull on stalbe 5

--- push_to_stable.sh
+++ push_to_stable.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-		cp -Rv build/ $HOME/build/
-        cp -Rv build/ $HOME/build/
+		cp -Rv . $HOME/./
+        cp -Rv . $HOME/./
        
+	    cd $HOME
         git config --global user.email "aurelien.lefebvre@viacesi.fr"
 		git config --global user.name "Travis"
         git config --global push.default matching
@@ -10,6 +11,7 @@
         git clone --quiet https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git repo > /dev/null || error_exit "Error cloning the repository";
 
         # checkout gh-pages
+		
         git checkout stable
         
         git pull origin master
