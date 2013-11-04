aa289cac420465e2a3a309c9c3c2ef016ec9a51e
Aurelien <aurelien.lefebvre@viacesi.fr>
2013-11-04 08:58:55 +0100

    Scrutinizer push

--- scrut.sh
+++ scrut.sh
@@ -3,11 +3,7 @@ git config --global user.email "aurelien.lefebvre@viacesi.fr"
 git config --global user.name "Travis"
 git config --global push.default matching
 
-curl https://scrutinizer-ci.com/g/alefebvre/Tool-box/inspections/b804ffab-9816-4828-9893-8b8d3f07f30c.diff?s=1a92acd8d95f2ec043e6e3bb7b79096f0327389b  \
-    | git apply -v
-	git add *
-	git commit -m "cs-fixer"
-	git push  https://${GH_TOKEN}@github.com/alefebvre/Tool-box.git
+git pull https://github.com/h4cc/scrutinizer patch-1
 	
 
 
